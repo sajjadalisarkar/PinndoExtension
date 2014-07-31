6b96632e794a106f27e613591d9f66a3ade1a735
sajjadalisarkar <sajjadalimail@gmail.com>
2014-08-01 03:08:58 +0530

    Added few event listeners

--- pinndoframe.js
+++ pinndoframe.js
@@ -1,6 +1,21 @@
+(function() {
+
+    var iframeId="iframe";
+    var iceframe = document.getElementById(iframeId);
+    if (iceframe) {
+        closeIceframe();
+        return;
+    }
     iframe = document.createElement('iframe');
+    iframe.id =iframeId ;
     //iframe.src ="http://localhost:63342/PinndoExtension/pinndoFrameSampleDOM.html";
-    iframe.src ="http://localhost:63342/PinndoExtension/pinndoframe.html";
+    iframe.src = "http://localhost:63342/PinndoExtension/pinndoframe.html";
     iframe.setAttribute("style", "visibility:visible;z-index: 1000000;position: fixed;top:5px; margin-bottom: 0px;margin-left: 0px;right:1px;width: 180px;height: 600px;border: none; overflow: hidden; border-radius:2px;");
     iframe.frameborder = "0";
-    document.body.appendChild(iframe)
+    document.body.appendChild(iframe);
+    function closeIceframe()
+    {
+        var iceframe = document.getElementById(iframeId);
+        document.body.removeChild(iceframe);
+    }
+})();
